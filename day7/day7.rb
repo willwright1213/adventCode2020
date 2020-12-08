b78c1e528b8a84fc8ca3999bfd4b753842fcb988
William Wright <william.wright@alumni.carleton.ca>
2020-12-07 22:22:38 -0500

    day 7 final

--- day7/day7.rb
+++ day7/day7.rb
@@ -1,11 +1,34 @@
 
+def findTheGoldenBag(root_bag, bag_color)
+  isFound = false;
+  
+  if(root_bag.fetch(bag_color).has_key?("shiny gold"))
+    isFound = true
+  end
 
-def findBagsInGoldenBag(root_bag, bag_color, val, sum)
-	root_bag.fetch(bag_color).each{|key, value|
-		value = value * val
-		sum = value + findBagsInGoldenBag(root_bag,key,value,sum)
-	}
-	return sum
+  root_bag.fetch(bag_color).each{ |key, value|
+    if(root_bag.has_key?(key))
+      while(!isFound)
+        if(findTheGoldenBag(root_bag, key)) 
+          isFound = true
+        end
+        break
+      end
+      if(isFound) 
+        return true
+      end
+    end
+  }
+  return false
+end
+
+
+def findBagsInGoldenBag(root_bag, bag_color, val = 1, sum = 0)
+  root_bag.fetch(bag_color).each{|key, value|
+    value = value * val
+    sum = value + findBagsInGoldenBag(root_bag,key,value,sum)
+  }
+  return sum
 end
 
 
@@ -19,12 +42,23 @@ File.foreach("input.txt"){ |line|
   number = line.scan(/\d+/)
 
   for i in 0..number.length - 1
-  	value = objects[4 + (i*4)]
-  	color = objects[5 + (i*4)] + " " + objects[6 + (i*4)]
-  	child_bag[color] = value.to_i
+    value = objects[4 + (i*4)]
+    color = objects[5 + (i*4)] + " " + objects[6 + (i*4)]
+    child_bag[color] = value.to_i
   end
 
   root_bag[root_bag_color] = child_bag
 
 
 }
+
+#itteration for part 1
+count = 0
+root_bag.each{|key, value|
+  if(findTheGoldenBag(root_bag, key))
+    count += 1
+  end
+}
+
+puts count
+puts findBagsInGoldenBag(root_bag, "shiny gold")
